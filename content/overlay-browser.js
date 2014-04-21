f4e08d7f7ed4ed2da5d69b529cf5fd570b61a5ff
Med0paW <medopaw@gmail.com>
2014-04-21 16:59:29 +0800

    Bug 1278 - Migrate to Australis

--- content/overlay-browser.js
+++ content/overlay-browser.js
@@ -4,85 +4,163 @@
 
 var ns = MOA.ns('ESS');
 ns.ceEasyScreenshot = {
-  handleEvent: function ce_easyscreenshot__handleEvent(aEvent) {
-    switch (aEvent.type) {
-      case "unload":
-        this.uninit();
-        break;
-      case "load":
-        setTimeout(this.init.bind(this), 500);
-        break;
-      case "TabSelect":
-      case "DOMContentLoaded":
-        this.updateUI(this.isHttp());
-        break;
+  buttonID: 'ce_easyscreenshot',
+  viewID: 'PanelUI-easyscreenshot-view',
+
+  strings: {
+    _bundle: null,
+    get: function(name) {
+      this._bundle = this._bundle || document.getElementById('easyscreenshot-strings');
+      return this._bundle.getString(name);
     }
   },
 
-  onCommand: function ce_easyscreenshot__onCommand(){
-    if(this.isHttp()){
+  handleEvent: function ce_easyscreenshot__handleEvent(aEvent) {
+    switch (aEvent.type) {
+      case "unload": {
+        this.uninit();
+        break;
+      }
+      case "load": {
+        setTimeout(this.init.bind(this), 500);
+         // On some page after restart, no event other than load is triggerd,
+         // thus icon will stay disabled no matter what shouldEnable() returns.
+         // The reason of this is still uncertain. Here's the dirty fix by
+         // manually checking shouldEnable() after 1 sec.
+        setTimeout(() => this.updateUI(aEvent), 1000);
+        break;
+      }
+      case "TabSelect":
+      case "DOMContentLoaded": {
+        this.updateUI(aEvent);
+        break;
+      }
+      case 'popupshowing': {
+        this.showSubViewFromArea(aEvent);
+        aEvent.preventDefault();
+        aEvent.stopPropagation();
+        break;
+      }
+      case 'ViewShowing': {
+        let {widget} = this.infoFromEvent(aEvent);
+        widget.anchor.setAttribute('open', 'true');
+        aEvent.target.removeEventListener('ViewShowing', this);
+        break;
+      }
+      case 'ViewHiding': {
+        let {widget} = this.infoFromEvent(aEvent);
+        widget.anchor.removeAttribute('open');
+        aEvent.target.removeEventListener('ViewHiding', this);
+        break;
+      }
+    }
+  },
+
+  onCommand: function ce_easyscreenshot__onCommand(aEvent){
+    if(this.shouldEnable(aEvent)){
       MOA.ESS.Snapshot.ssSelector();
     } else {
       MOA.ESS.Snapshot.getSnapshot('visible');
     }
   },
 
-  isHttp: function ce_easyscreenshot__isHttp(){
-    var tab = gBrowser.mCurrentTab;
-    var uri = null;
+  shouldEnable: function ce_easyscreenshot__shouldEnable(aEvent){
+    let {win} = this.infoFromEvent(aEvent);
+    let tab = win.gBrowser.mCurrentTab;
+    let uri = null;
     if(tab && tab.linkedBrowser)
       uri = tab.linkedBrowser.currentURI;
 
-    return (uri && (uri.scheme == "http" || uri.scheme == "https"));
+    // Button shouldn't be disabled on customization page.
+    let whitelist = ['about:customizing'];
+    return uri && (whitelist.indexOf(uri.spec) >= 0 || uri.schemeIs('http') || uri.schemeIs('https'));
   },
 
-  updateUI: function ce_easyscreenshot__updateUI(isHttp){
-    var btn =  document.getElementById("ce_easyscreenshot");
-//    var select = document.getElementById("easyscreenshot-snapshot-select");
-//    var entire = document.getElementById("easyscreenshot-snapshot-entire");
-    if(this.isHttp()){
-      btn.removeAttribute("disabled")
-//      select.setAttribute("disabled", "false")
-//      entire.setAttribute("disabled", "false")
-    } else {
-      btn.setAttribute("disabled", "true")
-//      select.setAttribute("disabled", "true")
-//      entire.setAttribute("disabled", "true")
+  updateUI: function ce_easyscreenshot__updateUI(aEvent){
+    let {widget} = this.infoFromEvent(aEvent);
+    let btn = widget.node;
+    if (btn) {
+      this.shouldEnable(aEvent) ?
+            btn.removeAttribute("disabled") :
+            btn.setAttribute("disabled", "true")
     }
   },
 
   init: function ce_easyscreenshot__init() {
-    this.installButton("ce_easyscreenshot", "nav-bar");
+    this.createButton();
+    document.getElementById('PanelUI-popup')
+            .addEventListener('popupshown',
+                              (aEvent) => this.updateUI(aEvent));
     gBrowser.tabContainer.addEventListener("TabSelect", this, false);
-  	window.addEventListener("DOMContentLoaded", this, false);
+    window.addEventListener("DOMContentLoaded", this, false);
   },
 
   uninit: function ce_easyscreenshot__init() {
-  	window.removeEventListener("DOMContentLoaded", this, false);
+    window.removeEventListener("DOMContentLoaded", this, false);
   },
 
-  installButton: function ce_easyscreenshot__installButton(buttonId,toolbarId) {
-    toolbarId = toolbarId || "addon-bar";
-    var key = "extensions.toolbarbutton.installed." + buttonId;
-    if (Application.prefs.getValue(key, false))
+  createButton: function ce_easyscreenshot__installButton() {
+    let widget = CustomizableUI.getWidget(this.buttonID);
+    if (widget && widget.provider == CustomizableUI.PROVIDER_API) {
       return;
+    }
 
-    var toolbar = window.document.getElementById(toolbarId);
-    let curSet = toolbar.currentSet;
-    if (-1 == curSet.indexOf(buttonId)) {
-      let newSet = curSet + "," + buttonId;
-      toolbar.currentSet = newSet;
-      toolbar.setAttribute("currentset", newSet);
-      document.persist(toolbar.id, "currentset");
-      try {
-        BrowserToolboxCustomizeDone(true);
-      } catch(e) {}
+    CustomizableUI.createWidget({
+      id: this.buttonID,
+      type: 'button',
+      defaultArea: CustomizableUI.AREA_NAVBAR,
+      label: this.strings.get('title'),
+      tooltiptext: this.strings.get('tooltip'),
+      onCreated: (aNode) => {
+        aNode.setAttribute('type', 'menu-button');
+        let doc = aNode.ownerDocument || document;
+        let menupopup = doc.createElement('menupopup');
+        menupopup.addEventListener('popupshowing', this);
+        aNode.appendChild(menupopup);
+      },
+      onCommand: (aEvent) => {
+        let {areaType} = this.infoFromEvent(aEvent);
+        if (areaType == CustomizableUI.TYPE_MENU_PANEL) {
+          this.showSubViewFromArea(aEvent, areaType);
+        } else {
+          this.onCommand(aEvent);
+        }
+      }
+    });
+  },
+
+  infoFromEvent: function(aEvent) {
+    let doc = aEvent.target && aEvent.target.ownerDocument || document;
+    let win = doc && doc.defaultView || window;
+    let widgetGroup = CustomizableUI.getWidget(this.buttonID);
+    return {
+      doc: doc,
+      win: win,
+      widget: widgetGroup.forWindow(win),
+      areaType: widgetGroup.areaType
+    };
+  },
+
+  showSubView: function(aWin, aAnchor, aArea) {
+    let view = aWin.document.getElementById(this.viewID);
+    view.addEventListener('ViewShowing', this);
+    view.addEventListener('ViewHiding', this);
+    aAnchor.setAttribute('closemenu', 'none');
+    aWin.PanelUI.showSubView(this.viewID, aAnchor, aArea);
+  },
+
+  showSubViewFromArea: function(aEvent, aAreaType) {
+    let {doc, win, widget, areaType} = this.infoFromEvent(aEvent);
+    if ((aAreaType || areaType) == CustomizableUI.TYPE_MENU_PANEL) {
+      this.showSubView(win, widget.node, CustomizableUI.AREA_PANEL);
+    } else {
+      CustomizableUI.hidePanelForNode(widget.node);
+      let dm = doc.getAnonymousElementByAttribute(widget.anchor, 'anonid', 'dropmarker');
+      let anchor = dm ?
+                   doc.getAnonymousElementByAttribute(dm, 'class', 'dropmarker-icon') :
+                   widget.anchor;
+      this.showSubView(win, anchor, CustomizableUI.AREA_NAVBAR);
     }
-    if (toolbar.getAttribute("collapsed") == "true") {
-      toolbar.setAttribute("collapsed", "false");
-    }
-    document.persist(toolbar.id, "collapsed");
-    Application.prefs.setValue(key, true);
   },
 
   getScreenShot: function() {
@@ -132,7 +210,7 @@ ns.ceEasyScreenshot = {
     var file = Components.classes["@mozilla.org/file/directory_service;1"]
                          .getService(Components.interfaces.nsIProperties)
                          .get("Desk", Components.interfaces.nsIFile);
-    var filename = _stringBundle.getFormattedString("screentShotFile", [iso8601FromDate(new Date()) + ".png"]);
+    var filename = _stringBundle.getFormattedString("screenshotFile", [iso8601FromDate(new Date()) + ".png"]);
     file.append(filename);
     file.createUnique(Components.interfaces.nsIFile.NORMAL_FILE_TYPE, 0666);
 
@@ -183,7 +261,7 @@ ns.ceEasyScreenshot = {
     } else if (Services.appinfo.OS == "Darwin") {
       runProc("/usr/bin/open", ['-a', 'Preview', file.path]);
     } else {
-      var message = _stringBundle.getFormattedString("screentShotSaved", [file.path]);
+      var message = _stringBundle.getFormattedString("screenshotSaved", [file.path]);
       alert(message)
     }
   },
