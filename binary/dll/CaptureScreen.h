3e5e218dc349de537ba1c025490bcd8edc303854
wdeng <wdeng@mozilla.com>
2015-05-26 08:01:17 +0800

    add screen capture binary code

---
+++ binary/dll/CaptureScreen.h
@@ -0,0 +1,7 @@
+// MathFuncsDll.h
+
+#include <Windows.h>
+
+#define FUNCSDLL_API __declspec(dllexport)
+
+void FUNCSDLL_API createBitMap(LPTSTR);
---
+++ binary/dll/dll.cpp
@@ -0,0 +1,6 @@
+// dll.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+
+
