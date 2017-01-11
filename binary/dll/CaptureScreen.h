63c54932a7bb6bd2b28c5c01f314d7ce476e2eda
Hector Zhao <bzhao@mozilla.com.cn>
2017-01-11 16:41:23 +0800

    Bug 2393: remove the binary source code
    We'll have to wait for the desktopCapture API

--- binary/dll/CaptureScreen.h
+++
@@ -1,7 +0,0 @@
-// MathFuncsDll.h
-
-#include <Windows.h>
-
-#define FUNCSDLL_API __declspec(dllexport)
-
-void FUNCSDLL_API createBitMap(LPTSTR);
--- binary/dll/dll.cpp
+++
@@ -1,6 +0,0 @@
-// dll.cpp : Defines the exported functions for the DLL application.
-//
-
-#include "stdafx.h"
-
-
